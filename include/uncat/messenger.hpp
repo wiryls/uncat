7a5843a6e4568b1c547aa0fc9a0c5e4b874f08c7
wiryls <wiryls@gmx.com>
2021-04-12 22:52:05 +0800

    Update messenger.hpp

--- include/uncat/messenger.hpp
+++ include/uncat/messenger.hpp
@@ -22,26 +22,21 @@ namespace uncat
         using name_t = K;
         template<typename E> using handler_t = std::function<void(E const&)>;
         template<typename E> using   group_t = std::unordered_map<name_t, handler_t<E>>;
-        template<typename E> using   event_t = find_t<remove_cvr_t<E>, T...>;
-        template<typename E> using    void_t = last_t<event_t<E>, void>;
-        template<typename E, bool C> using maybe_bool_t = last_t<event_t<E>, select_t<C, bool, void>>;
+        template<typename E> using   event_t =  find_t<remove_cvr_t<E>, T...>;
+        template<typename E> using    this_t = first_t<messenger, event_t<E>>;
 
     public:
-        template<typename E, bool S = false> maybe_bool_t<E, S>    add_handler(name_t name, handler_t<E> f);
-        template<typename E, bool S = false> maybe_bool_t<E, S> remove_handler(name_t name);
-        template<typename E>                       void_t<E>              send(E && event);
+        template<typename E> this_t<E> &    add_handler(name_t name, handler_t<E> handler);
+        template<typename E> this_t<E> & remove_handler(name_t name);
+        template<typename E> this_t<E> &           send(E && event);
+
+    public:
+        void wait();
 
     private:
-        template<typename E> bool add_handler_sync  (name_t name, handler_t<E> && handler);
-        template<typename E> void add_handler_async (name_t name, handler_t<E> && handler);
-        template<typename E> bool add_handler_unsafe(name_t name, handler_t<E>    handler);
-
-        template<typename E> bool remove_handler_sync  (name_t name);
-        template<typename E> void remove_handler_async (name_t name);
-        template<typename E> bool remove_handler_unsafe(name_t name);
-
-        template<typename E> void send_async (E      && event);
-        template<typename E> void send_unsafe(E const & event);
+        template<typename E> void    add_handler_unsafe(name_t const & name, handler_t<E> const & handler);
+        template<typename E> void remove_handler_unsafe(name_t const & name);
+        template<typename E> void           send_unsafe(E const & event);
 
     private:
         std::tuple<group_t<T>...> gorups; /// handlers is only used in one thread.
@@ -49,125 +44,74 @@ namespace uncat
     };
 
     template<typename K, typename ...T>
-    template<typename U, bool S> inline
-    typename messenger<K, T...>::template maybe_bool_t<U, S> messenger<K, T...>::
-    add_handler(name_t name, handler_t<U> handler)
+    template<typename U> inline
+    typename messenger<K, T...>::template this_t<U> & messenger<K, T...>::
+    add_handler(name_t name, handler_t<U> func)
     {
-        if constexpr (S)
-            return add_handler_sync <U>(name, std::move(handler));
-        else
-            /*  */ add_handler_async<U>(name, std::move(handler));
-    }
-
-    template<typename K, typename ...T>
-    template<typename U, bool S> inline
-    typename messenger<K, T...>::template maybe_bool_t<U, S> messenger<K, T...>::
-    remove_handler(name_t name)
-    {
-        if constexpr (S)
-            return remove_handler_sync <U>(name);
-        else
-            /*  */ remove_handler_async<U>(name);
+        todo.cross([this, name = std::move(name), func = std::move(func)]
+        {
+            add_handler_unsafe<U>(std::move(name), std::move(func));
+        });
+        return *this;
     }
 
     template<typename K, typename ...T>
     template<typename U> inline
-    typename messenger<K, T...>::template void_t<U> messenger<K, T...>::
+    typename messenger<K, T...>::template this_t<U> & messenger<K, T...>::
+    remove_handler(name_t name)
+    {
+        todo.cross([this, name = std::move(name)]
+        {
+            remove_handler_unsafe<U>(std::move(name));
+        });
+        return *this;
+    }
+
+    template<typename K, typename ...T>
+    template<typename U> inline
+    typename messenger<K, T...>::template this_t<U> & messenger<K, T...>::
     send(U && event)
-    {
-        send_async(std::forward<U>(event));
-    }
-
-    template<typename K, typename ...T>
-    template<typename U> inline bool messenger<K, T...>::
-    add_handler_sync(name_t name, handler_t<U> && func)
-    {
-        auto done = static_cast<bool>(func);
-        if (done)
-        {
-            auto sign = std::make_shared<std::promise<bool>>();
-            auto wait = sign->get_future();
-
-            done = todo.cross([this, name, sign = std::move(sign), func = std::move(func)]
-            {
-                auto o = add_handler_unsafe<U>(name, func);
-                sign->set_value(o);
-            });
-
-            if (done)
-                done = wait.get();
-        }
-        return done;
-    }
-
-    template<typename K, typename ...T>
-    template<typename U> inline void messenger<K, T...>::
-    add_handler_async(name_t name, handler_t<U> && func)
-    {
-        todo.cross([this, name, func = std::move(func)]
-        {
-            add_handler_unsafe<U>(name, func);
-        });
-    }
-
-    template<typename K, typename ...T>
-    template<typename U> inline bool messenger<K, T...>::
-    add_handler_unsafe(name_t name, handler_t<U> func)
-    {
-        auto& group = std::get<group_t<U>>(gorups);
-        group[name] = std::move(func);
-        return true;
-    }
-
-    template<typename K, typename ...T>
-    template<typename U> inline bool messenger<K, T...>::
-    remove_handler_sync(name_t name)
-    {
-        auto sign = std::make_shared<std::promise<bool>>();
-        auto wait = sign->get_future();
-        auto done = todo.cross([this, name, sign = std::move(sign)]
-        {
-            auto o = remove_handler_unsafe<U>(name);
-            sign->set_value(o);
-        });
-
-        if (done)
-            done = wait.get();
-
-        return done;
-    }
-
-    template<typename K, typename ...T>
-    template<typename U> inline void messenger<K, T...>::
-    remove_handler_async(name_t name)
-    {
-        todo.cross([this, name]
-        {
-            remove_handler_unsafe<U>(name);
-        });
-    }
-
-    template<typename K, typename ...T>
-    template<typename U> inline bool messenger<K, T...>::
-    remove_handler_unsafe(name_t name)
-    {
-        auto & group = std::get<group_t<U>>(gorups);
-        auto    iter = group.find(name);
-        auto    find = iter != group.end();
-        if (find)
-            group.erase(iter);
-
-        return find;
-    }
-
-    template<typename K, typename ...T>
-    template<typename U> inline void messenger<K, T...>::
-    send_async(U && event)
     {
         todo.cross([this, event = std::move(event)]
         {
             send_unsafe<remove_cvr_t<U>>(event);
         });
+        return *this;
+    }
+
+    template<typename K, typename ...T> inline
+    void messenger<K, T...>::
+    wait()
+    {
+        auto sign = std::make_shared<std::promise<void>>();
+        auto wait = sign->get_future();
+        auto done = todo.cross([sign = std::move(sign)]
+        {
+            sign->set_value();
+        });
+
+        if (done)
+            wait.get();
+    }
+
+    template<typename K, typename ...T>
+    template<typename U> inline void messenger<K, T...>::
+    add_handler_unsafe(name_t const & name, handler_t<U> const & func)
+    {
+        auto& group = std::get<group_t<U>>(gorups);
+        group[name] = func;
+    }
+
+    template<typename K, typename ...T>
+    template<typename U> inline void messenger<K, T...>::
+    remove_handler_unsafe(name_t const & name)
+    {
+        auto & group = std::get<group_t<U>>(gorups);
+        auto    iter = group.find(name);
+        auto    find = iter != group.end();
+
+        if (find)
+            group.erase(iter);
     }
 
     template<typename K, typename ...T>
